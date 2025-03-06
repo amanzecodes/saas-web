982c761f6e8a118936c1c2365c3aa4b7e1cb7bb1
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-06 17:33:36 +0100

    added over transition to the nav menu

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,8 @@ import { cva } from "class-variance-authority";
 const classes = cva("border h-12 rounded-full px-6 font-medium", {
     variants: {
         variant: {
-            primary: "bg-lime-400 text-neutral-950 border-lime-400",
-            secondary: "border-white text-white bg-transparent",
+           primary: "bg-lime-400 hover:bg-lime-300 text-neutral-950 border-lime-400 transition duration-300",
+            secondary: "border-white hover:font-bold text-white bg-transparent transition duration-500",
         },
         size: {
             "sm": "h-10"
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,24 @@
+import Image from "next/image";
 import Button from "@/components/Button";
+import designExample1Image from '@/assets/images/design-example-1.png';
+import designExample2Image from '@/assets/images/design-example-2.png';
+
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative ">
+                <div className="absolute -left-32 top-16">
+                    <Image src={designExample1Image} alt="Design Example 1" className="cursor-nesw-resize"/>
+                </div>
+                <div className="absolute -right-64 -top-16">
+                    <Image src={designExample2Image} alt="Design Example 2" className="cursor-nwse-resize"/>
+                </div>
                 <div className="flex justify-center items-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨$7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
                     Impactful design, created effortlessly
                 </h1>
                 <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
