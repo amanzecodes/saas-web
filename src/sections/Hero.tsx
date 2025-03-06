9e63c62ba74c9d2a4c877cde58d402da3d9d0d8c
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-06 16:47:16 +0100

    responsive hero section for medium devices

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,19 +8,19 @@ export default function Hero() {
                         ✨$7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl font-medium text-center mt-6">
+                <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">
                     Impactful design, created effortlessly
                 </h1>
-                <p className="text-center text-xl text-white/50 mt-8">
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
                     Design tools shouldn&apos;t slow you down. Layers combines
                     powerful features with an intuitive interface that keeps you
                     in your creative flow
                 </p>
-                <form className="flex flex-row border border-white/15 rounded-full p-2 mt-8">
+                <form className="flex flex-row border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
                     <input
                         type="email"
                         placeholder="Enter your email"
-                        className="bg-transparent px-4 min-w-0"
+                        className="bg-transparent px-4 md:flex-1 min-w-0 border-none rounded-full outline-none focus:ring-0 focus:border-transparent"
                     />
                     <Button
                         type="submit"
