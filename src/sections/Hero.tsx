0418467e62f8c5375af9ee0be5ef4c288e147c31
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-06 16:23:24 +0100

    created form for the mobile view

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,37 @@
+import Button from "@/components/Button";
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center items-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨$7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl font-medium text-center mt-6">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8">
+                    Design tools shouldn&apos;t slow you down. Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow
+                </p>
+                <form className="flex flex-row border border-white/15 rounded-full p-2 mt-8">
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="bg-transparent px-4 min-w-0"
+                    />
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="whitespace-nowrap"
+                        size="sm"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
 }
