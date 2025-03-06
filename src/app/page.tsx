4d250dc1eedc92b8e5de2aec7f71b11b068d2078
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-06 20:58:00 +0100

    done with the static introductory section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/sections/Hero";
 import Navbar from "@/sections/Navbar";
 import LogoTicker from "@/sections/LogoTicker";
+import Introduction from "@/sections/Introduction";
 
 export default function Home() {
     return <>
     <Navbar />
     <Hero />
     <LogoTicker />
+    <Introduction />
     </>
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
     return (
         <section className="py-4 lg:py-8">
             <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full px-4 md:pr-2 items-center mt-3">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full px-4 md:pr-2 items-center mt-3 py-2">
                     <div>
                         <Image src={logoImage} alt="Layers logo"className="h-9 md:h-auto w-auto"/>
                     </div>
