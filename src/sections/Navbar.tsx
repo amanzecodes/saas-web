52b5d92c718559d4a462822144a36636aff82191
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-06 09:19:43 +0100

    done with the responsive layout for the navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,5 +1,6 @@
 import logoImage from "@/assets/images/logo.svg";
 import Image from "next/image";
+import Button from "@/components/Button";
 
 
 const navLinks = [
@@ -11,13 +12,20 @@ const navLinks = [
 
 export default function Navbar() {
     return (
-        <section className="py-4">
-            <div className="container">
-                <div className="grid grid-cols-2 border border-white/15 rounded-full px-4 items-center">
+        <section className="py-4 lg:py-8">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full px-4 md:pr-2 items-center">
                     <div>
-                        <Image src={logoImage} alt="Layers logo"className="h-9 w-auto"/>
+                        <Image src={logoImage} alt="Layers logo"className="h-9 md:h-auto w-auto"/>
                     </div>
-                    <div className="flex justify-end">
+                    <div className="lg:flex justify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map(link => (
+                                <a href={link.href} key={link.label}>{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                    <div className="flex justify-end gap-4">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="24"
@@ -25,15 +33,17 @@ export default function Navbar() {
                             viewBox="0 0 24 24"
                             fill="none"
                             stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            className="feather feather-menu"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="feather feather-menu md:hidden"
                         >
                             <line x1="3" y1="12" x2="21" y2="12" />
                             <line x1="3" y1="6" x2="21" y2="6" />
                             <line x1="3" y1="18" x2="21" y2="18" />
                         </svg>
+                        <Button variant="secondary" className="hidden md:inline-flex items-center">Log In</Button>
+                        <Button variant="primary" className="hidden md:inline-flex items-center">Sign Up</Button>
                     </div>
                 </div>
             </div>
