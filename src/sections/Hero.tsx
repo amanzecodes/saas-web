36564538925229368997db7b256ffdddfec72566
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-06 18:16:36 +0100

    done with the responsive layout for the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,20 +1,33 @@
 import Image from "next/image";
 import Button from "@/components/Button";
-import designExample1Image from '@/assets/images/design-example-1.png';
-import designExample2Image from '@/assets/images/design-example-2.png';
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
 import Pointer from "@/components/pointer";
 
 export default function Hero() {
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container relative ">
-                <div className="absolute -left-32 top-16">
-                    <Image src={designExample1Image} alt="Design Example 1" className="cursor-nesw-resize"/>
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image
+                        src={designExample1Image}
+                        alt="Design Example 1"
+                        className="cursor-nesw-resize"
+                    />
                 </div>
-                <div className="absolute -right-64 -top-16">
-                    <Image src={designExample2Image} alt="Design Example 2" className="cursor-nwse-resize"/>
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image
+                        src={designExample2Image}
+                        alt="Design Example 2"
+                        className="cursor-nwse-resize"
+                    />
                 </div>
-                <div className="absolute"><Pointer name="Andrea" color="red"/></div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Bryan" color="red" />
+                </div>
                 <div className="flex justify-center items-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨$7.5M seed round raised
