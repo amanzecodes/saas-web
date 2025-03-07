7c693e56d5a87b7ff3de2b6d273906a0ace59386
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-07 16:25:10 +0100

    created the first feature card for mobile view

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,4 +1,10 @@
 import Tag from "@/components/Tag";
+import FeatureCard from "@/components/FeatureCard";
+import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg";
+import avatar2 from "@/assets/images/avatar-lula-meyers.jpg";
+import avatar3 from "@/assets/images/avatar-florence-shaw.jpg";
+import Image from "next/image";
+import Avatar from "@/components/Avatar";
 
 const features = [
     "Asset Library",
@@ -18,39 +24,59 @@ export default function Features() {
                     <Tag>Features</Tag>
                 </div>
                 <h2 className="font-medium text-6xl text-center mt-6">
-                    Where power meets <span className="text-lime-400">simplicity</span>
+                    Where power meets{" "}
+                    <span className="text-lime-400">simplicity</span>
                 </h2>
-                <div>
-                    <div>
-                        <div></div>
-                        <div>
-                            <h3>Real-time Collaboration</h3>
-                            <p>
-                                Work together seamlessly with conflic-free team
-                                editing
-                            </p>
+                <div className="mt-12 grid grid-cols-1 gap-8">
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="Work together seamlessly with conflict-free team
+                                editing"
+                    >
+                        <div className="aspect-video flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image
+                                    src={avatar1}
+                                    alt="Avatar 1"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image
+                                    src={avatar2}
+                                    alt="Avatar 2"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image
+                                    src={avatar3}
+                                    alt="Avatar 2"
+                                    className="rounded-full"
+                                />
+                            </Avatar>
+                            <Avatar className="-ml-6 z-10 border-transparent">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span
+                                            className="size-1.5 bg-white rounded-full inline-flex"
+                                            key={i}
+                                        ></span>
+                                    ))}
+                                </div>
+                            </Avatar>
                         </div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div>
-                            <h3>Interactive Prototyping</h3>
-                            <p>
-                                Engage your clients with prototypes that react
-                                to user actions
-                            </p>
-                        </div>
-                    </div>
-                    <div>
-                        <div></div>
-                        <div>
-                            <h3>Keyboard Quick Actions</h3>
-                            <p>
-                                Powerful commands to help you create designs
-                                more quickly
-                            </p>
-                        </div>
-                    </div>
+                    </FeatureCard>
+                    <FeatureCard
+                        title="Interactive Prototyping"
+                        description="Engage your clients with prototypes that react
+                                to user actions"
+                    />
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful commands to help you create designs
+                                more quickly"
+                    />
                 </div>
                 <div>
                     {features.map((feature, id) => (
