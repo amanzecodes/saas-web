f5b3e61dbe12411fc3cffcaab4a67037775639a6
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-09 00:25:30 +0100

    working on the faq section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import LogoTicker from "@/sections/LogoTicker";
 import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
+import Faqs from "@/sections/Faqs";
 
 export default function Home() {
     return <>
@@ -13,5 +14,6 @@ export default function Home() {
     <Introduction />
     <Features />
     <Integrations />
+    <Faqs />
     </>
 }
--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,3 +1,5 @@
+import Tag from "@/components/Tag";
+
 const faqs = [
     {
         question: "How is Layers different from other design tools?",
@@ -22,5 +24,41 @@ const faqs = [
 ];
 
 export default function Faqs() {
-    return <div>Faqs</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                <Tag>Faqs</Tag>
+                </div>
+                <h2>
+                    Questions? We've got <span>answers</span>
+                </h2>
+                <div>
+                    {faqs.map((faq, index) => (
+                        <div key={index}>
+                            <div>
+                                <h3>{faq.question}</h3>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="feather feather-plus text-lime-400"
+                                >
+                                    <line x1="12" y1="5" x2="12" y2="19" />
+                                    <line x1="5" y1="12" x2="19" y2="12" />
+                                </svg>
+                            </div>
+                            <p>{faq.answer}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
