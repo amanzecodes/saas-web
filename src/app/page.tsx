eff7465f0431813c2b4693667efa83423e303ace
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-09 17:10:26 +0100

    working on the cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Integrations from "@/sections/Integrations";
 import Faqs from "@/sections/Faqs";
+import CallToAction from "@/sections/CallToAction";
 
 export default function Home() {
     return <>
@@ -15,5 +16,6 @@ export default function Home() {
     <Features />
     <Integrations />
     <Faqs />
+    <CallToAction />
     </>
 }
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,12 @@
 export default function CallToAction() {
-    return <div>Call To Action</div>;
+    return (
+        <section>
+            {Array.from({ length: 10 }).map((_, i) => (
+                <div key={i}>
+                    <span>&#10038;</span>
+                    <span>Try it for free</span>
+                </div>
+            ))}
+        </section>
+    );
 }
