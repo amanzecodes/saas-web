3466ac266e5bdfe7893e6b89d34cb043db05eb96
Amanze Bruno <bamanze.2301387@stu.cu.edu.ng>
2025-03-09 23:14:26 +0100

    done with the left design animation in the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Button from "@/components/Button";
@@ -12,48 +12,65 @@ export default function Hero() {
     const [leftDesignScope, leftDesignAnimate] = useAnimate();
     const [leftPointerScope, leftPointerAnimate] = useAnimate();
 
+    const [rightDesignScope, rightDesignAnimate] = useAnimate();
+    const [rightPointerScope, rightPointerAnimate] = useAnimate();
+
     useEffect(() => {
         leftDesignAnimate([
-            [leftDesignScope.current, {opacity: [1]}, { duration:0.5}],
-            [leftDesignScope.current, {y: 0, x: 0}, { duration: 0.5}]
+            [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+
+        leftPointerAnimate([
+            [leftPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+            [leftPointerScope.current, { y: 0, x: -100 }, { duration: 0.5 }],
+            [leftPointerScope.current, { x: 0, y:[0, 16, 0]}, { duration: 0.5, ease:'easeInOut' }],
         ])
-    }, [])
+
+
+    }, []);
 
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container relative">
-
-
-                <motion.div 
-                initial={{
-                    opacity: 0,
-                    y: 100,
-                    x: -100
-                }}
-                ref={leftDesignScope} className="absolute -left-32 top-16 hidden lg:block">
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                        y: 100,
+                        x: -100,
+                    }}
+                    ref={leftDesignScope}
+                    className="absolute -left-32 top-16 hidden lg:block"
+                >
                     <Image
                         src={designExample1Image}
                         alt="Design Example 1"
                         className="cursor-nesw-resize"
                     />
                 </motion.div>
-                <motion.div ref={leftPointerScope} className="absolute left-56 top-96 hidden lg:block">
+                <motion.div
+                    ref={leftPointerScope}
+                    initial={{
+                        opacity: 0,
+                        y: 100,
+                        x: -200,
+                    }}
+                    className="absolute left-56 top-96 hidden lg:block"
+                >
                     <Pointer name="Andrea" />
                 </motion.div>
 
-
-                <div className="absolute -right-64 -top-16 hidden lg:block">
+                <motion.div className="absolute -right-64 -top-16 hidden lg:block">
                     <Image
                         src={designExample2Image}
                         alt="Design Example 2"
                         className="cursor-nwse-resize"
                     />
-                </div>
+                </motion.div>
 
-                <div className="absolute right-80 -top-4 hidden lg:block">
+                <motion.div className="absolute right-80 -top-4 hidden lg:block">
                     <Pointer name="Bryan" color="red" />
-                </div>
-
+                </motion.div>
 
                 <div className="flex justify-center items-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
